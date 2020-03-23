ef35bc6f71aa09ff06eebb876cd7d1c409281c5b
Adhwaith123 <62431227+Adhwaith123@users.noreply.github.com>
2020-03-23 17:57:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var movingRect,fixedRect;
+
+function setup() {
+  createCanvas(800,400);
+ movingRect= createSprite(400, 200, 50, 50);
+movingRect.shapeColor = "red";
+
+fixedRect= createSprite(200, 200, 50, 30);
+fixedRect.shapeColor = "red";
+
+}
+
+function draw() {
+  background("black"); 
+  movingRect.x = mouseX;
+  movingRect.y = mouseY; 
+
+  if(fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2
+    && movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2
+    &&fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2
+    &&movingRect.y-fixedRect.y<fixedRect.height/2+movingRect.height/2){
+    movingRect.shapeColor = "blue";
+    fixedRect.shapeColor = "blue";
+  }
+
+  else{
+   movingRect.shapeColor = "red";
+   fixedRect.shapeColor = "red";
+  }
+  drawSprites();
+}
